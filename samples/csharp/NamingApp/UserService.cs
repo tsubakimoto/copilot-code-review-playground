0e32a82f4b5e15bd4ae21c61436169e8285d1d08
Yuta Matsumura <tkmbmbr@gmail.com>
2025-11-16 15:02:51 +0900

    NamingAppプロジェクトの初期ファイルを追加

---
+++ samples/csharp/NamingApp/UserService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NamingApp
+{
+    internal interface UserService
+    {
+        string GetName(int userId);
+    }
+}
